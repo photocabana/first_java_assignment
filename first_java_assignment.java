fe0c71dd80c6fd37fd9a3acd00eb29a37cba2847
Rindy Fisk <117949294+photocabana@users.noreply.github.com>
2023-07-14 15:14:16 -0500

    Add files via upload

---
+++ first_java_assignment.java
@@ -0,0 +1,7 @@
+public class first_java_assignment {
+    public static void main(String[] args){
+        System.out.println("My name is Rindy Fisk");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is Edwardsville, IL");
+    }
+}
